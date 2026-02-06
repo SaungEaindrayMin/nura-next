a6bf0a51c7cbbcd9ad4938f0899ac6eca40b183f
Saung Eaindray Min <saungeaindraymin@gmail.com>
2026-02-07 00:22:05 +0630

    [FIX] service transition section details

--- src/components/sections/services/ServiceTransition.jsx
+++ src/components/sections/services/ServiceTransition.jsx
@@ -8,7 +8,6 @@ const ServiceTransition = () => {
   const controlsLine1 = useAnimationControls();
   const controlsLine2 = useAnimationControls();
 
-
   const line1 = ["We", "don't", "just", "build", "websites"];
   const line2 = [
     "We",
@@ -24,43 +23,38 @@ const ServiceTransition = () => {
   const subText =
     "Our job is to make it more human. To build digital products, that help businesses do better, be better and show up in a way that makes our consumers' lives better.";
 
-
   useEffect(() => {
     if (!inView) return;
 
     const run = async () => {
-      await controlsLine1.start(i => ({
+      await controlsLine1.start((i) => ({
         opacity: 1,
         color: "#00E3FE",
         transition: { duration: 0.45, delay: i * 0.12 },
       }));
 
-      await controlsLine2.start(i => ({
+      await controlsLine2.start((i) => ({
         opacity: 1,
         color: "#00E3FE",
         transition: { duration: 0.45, delay: i * 0.12 },
       }));
-
-
     };
 
     run();
   }, [inView, controlsLine1, controlsLine2]);
 
-
   return (
     <section
       ref={ref}
-      className="relative min-h-[100vh] flex items-center justify-center font-grotesk"
+      className="container-custom  relative min-h-[100vh] flex items-center justify-center font-grotesk"
     >
-      <div className="container-custom grid grid-cols-1 lg:grid-cols-2  items-center">
-
+      <div className="grid grid-cols-2 items-end">
         <div>
-          <h2 className="text-secondary text-lg font-bold tracking-widest mb-8 uppercase">
+          <h2 className="text-secondary text-xl font-bold tracking-widest mb-8 uppercase">
             What Do We Do?
           </h2>
 
-          <div className="flex flex-wrap gap-x-3 text-3xl md:text-4xl font-medium leading-tight mb-6">
+          <div className="flex flex-wrap gap-x-2 text-4xl md:text-5xl font-medium leading-tight mb-6">
             {line1.map((word, i) => (
               <motion.span
                 key={i}
@@ -77,8 +71,7 @@ const ServiceTransition = () => {
             ))}
           </div>
 
-          {/* LINE 2 */}
-          <div className="flex flex-wrap gap-x-3 text-4xl md:text-6xl font-medium leading-tight">
+          <div className="flex flex-wrap gap-x-2 text-4xl md:text-5xl font-medium leading-tight">
             {line2.map((word, i) => (
               <motion.span
                 key={i}
@@ -96,9 +89,7 @@ const ServiceTransition = () => {
           </div>
         </div>
 
-        <motion.div
-          className="text-white text-lg md:text-xl  leading-relaxed max-w-md lg:ml-auto"
-        >
+        <motion.div className="text-white text-lg md:text-xl leading-relaxed ">
           {subText}
         </motion.div>
       </div>
